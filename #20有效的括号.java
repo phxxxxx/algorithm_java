7a7a77e1d1ffe23d3f97fb9ee3957ad69ab4544a
phxxxxx <wuk.jlu@outlook.com>
2021-03-20 22:12:50 +0800

    完成 20 222 297

---
+++ #20有效的括号.java
@@ -0,0 +1,59 @@
+/*
+20. 有效的括号
+给定一个只包括 '('，')'，'{'，'}'，'['，']' 的字符串 s ，判断字符串是否有效。
+
+有效字符串需满足：
+
+左括号必须用相同类型的右括号闭合。
+左括号必须以正确的顺序闭合。
+ 
+
+示例 1：
+
+输入：s = "()"
+输出：true
+示例 2：
+
+输入：s = "()[]{}"
+输出：true
+示例 3：
+
+输入：s = "(]"
+输出：false
+示例 4：
+
+输入：s = "([)]"
+输出：false
+示例 5：
+
+输入：s = "{[]}"
+输出：true
+ 
+
+提示：
+
+1 <= s.length <= 104
+s 仅由括号 '()[]{}' 组成
+*/
+
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack();
+        for(int i = 0;i < s.length();i++){
+            if(s.charAt(i)=='(' ||s.charAt(i) == '['|| s.charAt(i)=='{'){
+                stack.push(s.charAt(i));
+            }else
+            if(!stack.empty() && stack.peek() == convert(s.charAt(i))){
+                stack.pop();
+            }else{
+                return false;
+            }
+        }
+       return stack.empty();
+    }
+    public char convert(char c){
+        if(c == '}') return '{';
+        if(c == ']') return '[';
+        return '(';
+    }
+}
